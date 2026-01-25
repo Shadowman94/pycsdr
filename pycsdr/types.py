ced905a894ca818157a5528faa04c8562e6ecbe0
Marat Fayzullin <luarvique@gmail.com>
2026-01-25 15:29:35 -0500

    Slowing AGC attack values to avoid clicking.

--- pycsdr/types.py
+++ pycsdr/types.py
@@ -11,10 +11,10 @@ class Format(Enum):
 
 
 class AgcProfile(Enum):
-    SLOW = ("Slow", 0.01, 0.0001, 600)
-    LAG  = ("Laggy", 0.1, 0.00008, 550)
-    MID  = ("Mid", 0.1, 0.00045, 300)
-    FAST = ("Fast", 0.1, 0.001, 200)
+    SLOW = ("Slow", 0.005, 0.0001, 600)
+    LAG  = ("Laggy", 0.01, 0.0001, 550)
+    MID  = ("Mid", 0.01, 0.00045, 300)
+    FAST = ("Fast", 0.01, 0.001, 200)
 
     def __new__(cls, name: str, attack: float, decay: float, hangTime: int):
         obj = object.__new__(cls)
